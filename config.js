7965109252d8c0f014dba65f7f0569572fcb46e6
Connie <clai011@ucr.edu>
2019-11-13 10:24:36 -0800

    changed styling and added React Router(BrowserRouter)

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    PG_URI: 'postgres://oilyenjf:r8FSjtU5ixNsV5GZ2_eq64lzlkMv6uwR@salt.db.elephantsql.com:5432/oilyenjf'
+}
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const LoginPage = ({ signupButton }) => {
-    return ( 
-        <div>
-            <form action="/dbRouter/login" method="POST">
-                username: <input type="text" name="username" required/><br/>
-                password: <input type="password" name="password" required/><br/>
-                <input id="signup" type="submit" value="Login"></input>
+    return (
+        <div className="container bg1">
+            <form action="/dbRouter/login" method="POST" className="flex f-col">
+                username: <input type="text" name="username" required /><br />
+                password: <input type="password" name="password" required /><br />
+                <Button id="signup" variant="dark">Login</Button>
+                {/* <input id="signup" type="submit" value="Login" /> */}
             </form>
-            <button onClick={ signupButton }>Sign-Up Here!</button>
+            <a href="/signup" onClick={signupButton}>
+                <Button variant="link" className="auth-btn">Sign up here.</Button>
+            </a>
         </div>
-     );
+    );
 }
- 
+
 export default LoginPage;
