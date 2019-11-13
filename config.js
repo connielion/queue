bd1d06c643f5139fef5cda3160d1a890e815cc60
Connie <clai011@ucr.edu>
2019-11-13 11:52:52 -0800

    pasted content from Sung

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
+    googleAPI: 'AIzaSyBjccnaR4mB4VCdVmdd7IjMmxpMfAEQ-Gs',
     PG_URI: 'postgres://oilyenjf:r8FSjtU5ixNsV5GZ2_eq64lzlkMv6uwR@salt.db.elephantsql.com:5432/oilyenjf'
 }
