036377242c417fcce04260a19a96231d4590812a
Connie <clai011@ucr.edu>
2019-11-14 11:16:47 -0800

    styled chat

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios'
-const SignUpPage = ({ loginButton }) => {
-    function postToSignup() {
-        console.log(`postToSignup() clicked`)
-        axios.post(`/dbRouter/signup`)
-            .then(res => {
-                console.log(`res from POST to /login`, res)
-            })
-    }
+const SignUpPage = (props) => {
+    const { signupButton } = props;
+    // getValue = function (e) {
+    //     console.log(e.target.value)
+    // // }
+    // postToSignup = function () {
+    //     console.log(`postToSignup() clicked`)
+    //     axios.post(`/dbRouter/signup`)
+    //         .then(res => {
+    //             console.log(`res from POST to /login`, res)
+    //         })
+    // }
+
+    // const getUsername = getValue()
+    // const getPassword = getValue()
     return (
         <div className="container bg1">
+            <h1>Sign Up</h1>
             <form action="/dbRouter/signup" method="POST" className="flex f-col">
-                username: <input type="text" name="username" required /><br />
-                password: <input type="password" name="password" required /><br />
-                <Button id="signup" variant="dark" onClick={postToSignup}>Sign up</Button>
+                Username: <input type="text" name="username" required /><br />
+                Password: <input type="password" name="password" required autoComplete="true" /><br />
+                <Button id="signup" variant="dark" onClick={(e) => signupButton} >Sign up</Button>
             </form>
-            <a href="/login" onClick={loginButton}>
+            <a href="/login" >
                 <Button variant="link" className="auth-btn">Login here.</Button>
             </a>
         </div>
     );
+
 }
 
 export default SignUpPage;
